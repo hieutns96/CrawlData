18606f622805ca8d16cac34b6da82a08ba1ed044
Trinh.Pho <trinh.pho@watasolutions.com>
2019-08-12 22:43:12 +0700

    update paging, locations

--- CrawlData/CrawlExtension.cs
+++ CrawlData/CrawlExtension.cs
@@ -13,32 +13,36 @@ namespace CrawlData
             var htmlDoc = web.Load(url);
 
             var nodes = htmlDoc.DocumentNode.SelectNodes("//ul/li/div/div[contains(@class, 'searchResult__373c0__2UyNw')]");
-            foreach (var node in nodes)
+            //TODO: Remove "Sponsored Results"
+            if (nodes != null)
             {
-               
-                var html = node.OuterHtml;
-                var doc = new HtmlDocument();
-                doc.LoadHtml(html);
-
-                //get node data
-                var name = doc.DocumentNode.SelectSingleNode("//h3/a");
-                var rating = doc.DocumentNode.SelectSingleNode(
-                    "//div/span/div[contains(@class, 'lemon--div__373c0__1mboc')]");
-                var address = doc.DocumentNode.SelectSingleNode(
-                    "//div[contains(@class,'lemon--div__373c0__1mboc container__373c0__19wDx u-padding-l2 border-color--default__373c0__2oFDT')]");
-                var phone = doc.DocumentNode.SelectSingleNode(
-                    "//div/div/p[contains(@class, 'lemon--p__373c0__3Qnnj')]");
-                //parse data to object
-                if (name != null)
+                foreach (var node in nodes)
                 {
-                    var responseData = new ResponseData();
-                    responseData.Rating = rating.Attributes["aria-label"].Value;
-                    responseData.Name = name.InnerHtml ;
-                    responseData.Address = address.InnerText.Replace(phone.InnerHtml, "");
-                    responseData.Phone = phone.InnerHtml;
-                    responsesData.Add(responseData);
-                }
 
+                    var html = node.OuterHtml;
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(html);
+
+                    //get node data
+                    var name = doc.DocumentNode.SelectSingleNode("//h3/a");
+                    var rating = doc.DocumentNode.SelectSingleNode(
+                        "//div/span/div[contains(@class, 'lemon--div__373c0__1mboc')]");
+                    var address = doc.DocumentNode.SelectSingleNode(
+                        "//div[contains(@class,'lemon--div__373c0__1mboc container__373c0__19wDx u-padding-l2 border-color--default__373c0__2oFDT')]");
+                    var phone = doc.DocumentNode.SelectSingleNode(
+                        "//div/div/p[contains(@class, 'lemon--p__373c0__3Qnnj')]");
+                    //parse data to object
+                    if (name != null)
+                    {
+                        var responseData = new ResponseData();
+                        responseData.Rating = rating?.Attributes["aria-label"]?.Value;
+                        responseData.Name = name?.InnerHtml;
+                        responseData.Address = address?.InnerText.Replace(phone.InnerHtml, "");
+                        responseData.Phone = phone?.InnerHtml;
+                        responsesData.Add(responseData);
+                    }
+
+                }
             }
             return responsesData.ToArray();
         }
--- CrawlData/Program.cs
+++ CrawlData/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CrawlData
 {
@@ -6,17 +7,37 @@ namespace CrawlData
     {
         static void Main(string[] args)
         {
-            var html = @"https://www.yelp.com/search?find_desc=Wheel+Repair&find_loc=Austin%2C+TX&ns=";
-            var data = CrawlExtension.getContent(html);
-            foreach (var VARIABLE in data)
+            if (File.Exists("locations.txt"))
             {
-                Console.WriteLine(VARIABLE.Name);
-                Console.WriteLine(VARIABLE.Address);
-                Console.WriteLine(VARIABLE.Phone);
-                Console.WriteLine(VARIABLE.Rating);
-                Console.WriteLine("--------------------");
+                // Read a text file line by line.
+                string[] lines = File.ReadAllLines("locations.txt");//Write to a file
+                using (StreamWriter writer = new StreamWriter("data.txt"))
+                {
+                    foreach (string location in lines)
+                    {
+                        var start = 0;
+                        var countResult = 0;
+                        do
+                        {
+                            var html = $"https://www.yelp.com/search?find_desc=Wheel+Repair&find_loc={location}&ns=&start={start}";
+                            var data = CrawlExtension.getContent(html);
+                            foreach (var VARIABLE in data)
+                            {
+                                Console.WriteLine(VARIABLE.Name);
+                                Console.WriteLine(VARIABLE.Address);
+                                Console.WriteLine(VARIABLE.Phone);
+                                Console.WriteLine(VARIABLE.Rating);
+                                Console.WriteLine("--------------------");
+                                writer.WriteLine($"{VARIABLE.Name}/{VARIABLE.Address}/{VARIABLE.Phone}/{VARIABLE.Rating}");
+                            }
+                            
+                            countResult = data.Length;
+                            start += 10;
+                        }
+                        while (countResult != 0);
+                    }
+                }
             }
-           
         }
     }
 }
